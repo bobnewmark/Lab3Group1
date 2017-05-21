121ab179431714b57bb2eaba4b454f5c0f125237
bobnewmark <bobnewmark@ukr.net>
2017-05-22 02:21:10 +0300

    added "Phones" page (some problem with contexPath) and some pics

--- webTier-war/src/main/java/com/lab/account/web/ObjectController.java
+++ webTier-war/src/main/java/com/lab/account/web/ObjectController.java
@@ -12,9 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -32,30 +34,55 @@ public class ObjectController {
     @Autowired
     private ParameterService parameterService;
 
-    @RequestMapping(value = "/phones", method = RequestMethod.GET)
-    public ResponseEntity<List<Object>> allPhones() {
-        //CREATING OBJECT TYPE FOR SMARTPHONES
+//    @RequestMapping(value = "/phones", method = RequestMethod.GET)
+//    public ResponseEntity<List<Object>> allPhones() {
+//        //CREATING OBJECT TYPE FOR SMARTPHONES
+//        objectTypeService.save(new ObjectType("Smartphone", null, null));
+//        //CREATING ATTRIBUTES
+//        attributeService.save(new Attribute("price", objectTypeService.findByName("Smartphone").get(0)));
+//        attributeService.save(new Attribute("info", objectTypeService.findByName("Smartphone").get(0)));
+//        //CREATING TWO SMARTPHONES
+//        objectService.save(new Object("HTC", objectTypeService.findByName("Smartphone").get(0), null));
+//        objectService.save(new Object("Nokia", objectTypeService.findByName("Smartphone").get(0), null));
+//        //FILLING PARAMETERS FOR THEM
+//        parameterService.save(new Parameter(objectService.findByName("HTC").get(0), attributeService.findByName("price").get(0), "50$"));
+//        parameterService.save(new Parameter(objectService.findByName("HTC").get(0), attributeService.findByName("info").get(0), "Nice phone, good battery"));
+//        parameterService.save(new Parameter(objectService.findByName("Nokia").get(0), attributeService.findByName("price").get(0), "60$"));
+//        parameterService.save(new Parameter(objectService.findByName("Nokia").get(0), attributeService.findByName("info").get(0), "Nice phone, good display"));
+//
+//        List<Object> phones = objectService.findByObjectType(objectTypeService.findByName("Smartphone").get(0));
+//        if (phones.isEmpty()) {
+//            return new ResponseEntity<>(HttpStatus.NO_CONTENT);//You many decide to return HttpStatus.NOT_FOUND
+//        }
+//
+//        return new ResponseEntity<>(phones, HttpStatus.OK);
+//    }
+
+    @RequestMapping(value = {"/phones", "/laba/phones"}, method = RequestMethod.GET)
+    public String showPage(Model model) {
+
         objectTypeService.save(new ObjectType("Smartphone", null, null));
-        //CREATING ATTRIBUTES
+
         attributeService.save(new Attribute("price", objectTypeService.findByName("Smartphone").get(0)));
         attributeService.save(new Attribute("info", objectTypeService.findByName("Smartphone").get(0)));
-        //CREATING TWO SMARTPHONES
-        objectService.save(new Object("HTC", objectTypeService.findByName("Smartphone").get(0), null));
-        objectService.save(new Object("Nokia", objectTypeService.findByName("Smartphone").get(0), null));
-        //FILLING PARAMETERS FOR THEM
-        parameterService.save(new Parameter(objectService.findByName("HTC").get(0), attributeService.findByName("price").get(0), "50$"));
-        parameterService.save(new Parameter(objectService.findByName("HTC").get(0), attributeService.findByName("info").get(0), "Nice phone, good battery"));
-        parameterService.save(new Parameter(objectService.findByName("Nokia").get(0), attributeService.findByName("price").get(0), "60$"));
-        parameterService.save(new Parameter(objectService.findByName("Nokia").get(0), attributeService.findByName("info").get(0), "Nice phone, good display"));
+        attributeService.save(new Attribute("icon", objectTypeService.findByName("Smartphone").get(0)));
 
-        List<Object> phones = objectService.findByObjectType(objectTypeService.findByName("Smartphone").get(0));
-        if (phones.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);//You many decide to return HttpStatus.NOT_FOUND
-        }
+        objectService.save(new Object("Apple Iphone 6S Gold", objectTypeService.findByName("Smartphone").get(0), null));
+        objectService.save(new Object("LG K500 X VIEW", objectTypeService.findByName("Smartphone").get(0), null));
 
-        return new ResponseEntity<>(phones, HttpStatus.OK);
+        parameterService.save(new Parameter(objectService.findByName("Apple Iphone 6S Gold").get(0), attributeService.findByName("icon").get(0), "${contextPath}/resources/images/phones/APPLE IPHONE 6S 128GB GOLD.jpg"));
+        parameterService.save(new Parameter(objectService.findByName("Apple Iphone 6S Gold").get(0), attributeService.findByName("price").get(0), "950$"));
+        parameterService.save(new Parameter(objectService.findByName("Apple Iphone 6S Gold").get(0), attributeService.findByName("info").get(0), "Nice phone for those who don't like android"));
+        parameterService.save(new Parameter(objectService.findByName("LG K500 X VIEW").get(0), attributeService.findByName("icon").get(0), "${contextPath}/resources/images/phones/LG K500 X VIEW WHITE.jpg"));
+        parameterService.save(new Parameter(objectService.findByName("LG K500 X VIEW").get(0), attributeService.findByName("price").get(0), "60$"));
+        parameterService.save(new Parameter(objectService.findByName("LG K500 X VIEW").get(0), attributeService.findByName("info").get(0), "Nice phone, good display"));
+
+
+
+        List<Object> list = objectService.findByObjectType(objectTypeService.findByName("Smartphone").get(0));
+        model.addAttribute("allPhones", list);
+        return "phones";
     }
-
 
 
 }
--- webTier-war/src/main/webapp/WEB-INF/views/phones.jsp
+++ webTier-war/src/main/webapp/WEB-INF/views/phones.jsp
@@ -17,27 +17,79 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
 </head>
-<body ng-app="myApp" class="ng-cloak" ng-controller="PhonesCtrl as ctrl">
+<body ng-app="myApp" class="ng-cloak" ng-controller="PhoneController as ctrl">
 
-<div class="tablecontainer">
-    <table class="table table-hover">
-        <thead>
+<nav class="navbar navbar-inverse" role="navigation">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Lab 3</a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Главная</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Каталог<span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li ng-repeat="u in ctrl.phones"><a href="#"><span ng-bind="u.brand"></span><span
+                                ng-bind="u.model"></span></a></li>
+                    </ul>
+                </li>
+                <li><a href="/laba/phones">Phones</a></li>
+            </ul>
+            <ul class="nav navbar-nav pull-right">
+                <li class="pull-right"><a href="#" data-toggle="modal" data-target="#myModal">Вход</a></li>
+            </ul>
+            <div class="col-sm-3 col-md-3  pull-right">
+                <form class="navbar-form" role="search">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search" name="q">
+                        <div class="input-group-btn">
+                            <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+
+<table>
+    <tr>
+        <th>Details</th>
+        <th>Picture</th>
+    </tr>
+
+    <c:forEach items="${allPhones}" var="phone">
         <tr>
-            <th>ID.</th>
-            <th>Brand</th>
 
-            <th width="20%"></th>
+
+            <td>
+                    ${phone.name}<br>
+                    ${phone.objectType.name}<br>
+            </td>
+            <c:forEach items="${phone.parameters}" var="paramm">
+                <td>
+                    <c:if test="${paramm.attribute.name=='icon'}">
+                        icon!!!
+                        <div>
+                            <img src="${paramm.value}" alt="...oops, cannot find picture...">
+                        </div>
+                    </c:if>
+                </td>
+            </c:forEach>
         </tr>
-        </thead>
-        <tbody>
-        <tr ng-repeat="u in ctrl.phones">
-            <td><span ng-bind="u.id"></span></td>
+    </c:forEach>
 
-        </tr>
-        </tbody>
-    </table>
-</div>
-
+</table>
 
 
 </body>
